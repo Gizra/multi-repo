beaf6dd5fbe817b95aa0b253bee5650cc136e272
Amitai Burstein <amitai@gizra.com>
2020-02-13 02:05:08 +0200

    Add profile and config

--- web/sites/default/default.settings.php
+++ web/sites/default/default.settings.php
@@ -755,3 +755,4 @@ $settings['entity_update_backup'] = TRUE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
