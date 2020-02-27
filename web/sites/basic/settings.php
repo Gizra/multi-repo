22a7f3650ba4ad3f66313d079970446101745d59
Amitai Burstein <amitai@gizra.com>
2020-02-27 14:37:23 +0200

    Add `basic` as submodule

--- web/sites/basic/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-include $app_root . '/sites/default/settings.php';
-if (file_exists($app_root . '/sites/default/settings.ddev.php')) {
-  include $app_root . '/sites/default/settings.ddev.php';
-}
-$databases['default']['default']['database'] = 'basic';
-$config['config_split.config_split.basic']['status'] = TRUE;
